ea48e8282060cb77b9c1275abff875556c0194b1
fijixxx <20085584+fijixxx@users.noreply.github.com>
2020-12-06 21:11:30 +0900

    Feature #17 modify colorlist (#6)
    * #17 色リスト追加とランダムintの幅を調整
    
    * #17 typo
    
    * #17 typoo

--- src/create_meta.py
+++ src/create_meta.py
@@ -88,11 +88,12 @@ def lambda_handler(event, context):
     try:
         table = dynamodb.Table('sublog')
         with table.batch_writer() as batch:
-            color_name_list = ['pink', 'tomato', 'orange', 'plum', 'tan']
+            color_name_list = ['Indianred', 'Lightcoral', 'Salmon', 'Darksalmon', 'Lightsalmon', 'Crimson', 'Red', 'Firebrick', 'Darkred', 'Pink', 'Lightpink', 'Hotpink', 'Deeppink', 'Mediumvioletred', 'Palevioletred', 'Lightsalmon', 'Coral', 'Tomato', 'Orangered', 'Darkorange', 'Orange', 'Gold', 'Yellow', 'Lightyellow', 'Lemonchiffon', 'Lightgoldenrodyellow', 'Papayawhip', 'Moccasin', 'Peachpuff', 'Palegoldenrod', 'Khaki', 'Darkkhaki', 'Greenyellow', 'Chartreuse', 'Lawngreen', 'Lime', 'Limegreen', 'Palegreen', 'Lightgreen', 'Mediumspringgreen', 'Springgreen', 'Mediumseagreen', 'Seagreen', 'Forestgreen', 'Green', 'Darkgreen', 'Yellowgreen', 'Olivedrab', 'Olive', 'Darkolivegreen', 'Mediumaquamarine', 'Darkseagreen', 'Lightseagreen', 'Darkcyan', 'Teal', 'Aqua', 'Cyan', 'Lightcyan', 'Paleturquoise', 'Aquamarine', 'Turquoise', 'Mediumturquoise', 'Darkturquoise', 'Cadetblue', 'Steelblue', 'Lightsteelblue', 'Powderblue', 'Lightblue',
+                               'Skyblue', 'Lightskyblue', 'Deepskyblue', 'Dodgerblue', 'Cornflowerblue', 'Mediumslateblue', 'Royalblue', 'Blue', 'Mediumblue', 'Darkblue', 'Navy', 'Midnightblue', 'Lavender', 'Thistle', 'Plum', 'Violet', 'Orchid', 'Fuchsia', 'Magenta', 'Mediumorchid', 'Mediumpurple', 'Rebeccapurple', 'Blueviolet', 'Darkviolet', 'Darkorchid', 'Darkmagenta', 'Purple', 'Indigo', 'Slateblue', 'Darkslateblue', 'Mediumslateblue', 'Cornsilk', 'Blanchedalmond', 'Bisque', 'Navajowhite', 'Wheat', 'Burlywood', 'Tan', 'Rosybrown', 'Sandybrown', 'Goldenrod', 'Darkgoldenrod', 'Peru', 'Chocolate', 'Saddlebrown', 'Sienna', 'Brown', 'Maroon', 'Snow', 'Honeydew', 'Mintcream', 'Azure', 'Aliceblue', 'Ghostwhite', 'Whitesmoke', 'Seashell', 'Beige', 'Oldlace', 'Floralwhite', 'Ivory', 'Antiquewhite', 'Linen', 'Lavenderblush', 'Mistyrose', 'Gainsboro', 'Lightgray', 'Silver', 'Darkgray', 'Gray', 'Dimgray', 'Lightslategray', 'Slategray', 'Darkslategray', 'Black']
             for item in create_dict_list:
                 raw_curr_date = datetime.datetime.now()
                 str_curr_date = raw_curr_date.strftime('%Y-%m-%d %H:%M:%S')
-                color_name = color_name_list[random.randint(0, 4)]
+                color_name = color_name_list[random.randint(0, 141)]
 
                 id = str(uuid.uuid4())
                 category = item['category']
