782621d26ea4e9c525caecd3e012c15bfd71c244
yfijixxx <20085584+fijixxx@users.noreply.github.com>
2020-12-06 21:00:38 +0900

    #17 色リスト追加とランダムintの幅を調整

--- src/create_meta.py
+++ src/create_meta.py
@@ -88,11 +88,12 @@ def lambda_handler(event, context):
     try:
         table = dynamodb.Table('sublog')
         with table.batch_writer() as batch:
-            color_name_list = ['pink', 'tomato', 'orange', 'plum', 'tan']
+            color_name_list = ['Mediumseagreen', 'Seagreen', 'Forestgreen', 'Green', 'Darkgreen', 'Yellowgreen', 'Olivedrab', 'Olive', 'Darkolivegreen', 'Mediumaquamarine', 'Darkseagreen', 'Lightseagreen', 'Darkcyan', 'Teal', 'Aqua', 'Cyan', 'Lightcyan', 'Paleturquoise', 'Aquamarine', 'Turquoise', 'Mediumturquoise', 'Darkturquoise', 'Cadetblue', 'Steelblue', 'Lightsteelblue', 'Powderblue', 'Lightblue', 'Skyblue', 'Lightskyblue', 'Deepskyblue', 'Dodgerblue', 'Cornflowerblue', 'Mediumslateblue', 'Royalblue', 'Blue', 'Mediumblue', 'Darkblue', 'Navy', 'Midnightblue', 'Lavender', 'Thistle', 'Plum', 'Violet', 'Orchid', 'Fuchsia', 'Magenta', 'Mediumorchid', 'Mediumpurple',
+                               'Rebeccapurple', 'Blueviolet', 'Darkviolet', 'Darkorchid', 'Darkmagenta', 'Purple', 'Indigo', 'Slateblue', 'Darkslateblue', 'Mediumslateblue', 'Cornsilk', 'Blanchedalmond', 'Bisque', 'Navajowhite', 'Wheat', 'Burlywood', 'Tan', 'Rosybrown', 'Sandybrown', 'Goldenrod', 'Darkgoldenrod', 'Peru', 'Chocolate', 'Saddlebrown', 'Sienna', 'Brown', 'Maroon', 'Snow', 'Honeydew', 'Mintcream', 'Azure', 'Aliceblue', 'Ghostwhite', 'Whitesmoke', 'Seashell', 'Beige', 'Oldlace', 'Floralwhite', 'Ivory', 'Antiquewhite', 'Linen', 'Lavenderblush', 'Mistyrose', 'Gainsboro', 'Lightgray', 'Silver', 'Darkgray', 'Gray', 'Dimgray', 'Lightslategray', 'Slategray', 'Darkslategray', 'Black']
             for item in create_dict_list:
                 raw_curr_date = datetime.datetime.now()
                 str_curr_date = raw_curr_date.strftime('%Y-%m-%d %H:%M:%S')
-                color_name = color_name_list[random.randint(0, 4)]
+                color_name = color_name_list[random.randint(0, 100)]
 
                 id = str(uuid.uuid4())
                 category = item['category']
