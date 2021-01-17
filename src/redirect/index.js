d7e0c6b9c61131b70a7308fdbdb8cd41ccea9dc9
fijixxx <20085584+fijixxx@users.noreply.github.com>
2021-01-17 22:51:58 +0900

    #27 typo

--- src/redirect/index.js
+++ src/redirect/index.js
@@ -1,7 +1,8 @@
 exports.handler = async (event, context, callback) => {
     const request = event.Records[0].cf.request
     request.uri =
-        request.uri.match(/^\/article\/*/) | request.uri.match(/^\/category\/*/)
+        request.uri.match(/^\/article\/*/) ||
+        request.uri.match(/^\/category\/*/)
             ? request.uri + '.html'
             : request.uri
     console.log(request.uri)
